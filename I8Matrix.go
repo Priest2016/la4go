a0ef7997c85e1e2dff7242907cbc8b45ae1a0153
Priest2016 <priestrussian@gmail.com>
2018-03-29 02:30:04 +0300

    I8Matrix

--- I8Matrix.go
+++ I8Matrix.go
@@ -1,5 +1,9 @@
 package la4go
 
+import (
+	"errors"
+)
+
 type I8Matrix struct {
 	n    int
 	m    int
@@ -14,4 +18,45 @@ func NewI8Matrix(n, m int) I8Matrix {
 	}
 
 	return I8Matrix{n, m, data}
+}
+
+func (i8m *I8Matrix) GetN() int {
+	return i8m.n
+}
+
+func (i8m *I8Matrix) GetM() int {
+	return i8m.m
+}
+
+func (i8m *I8Matrix) GetData() [][]int8 {
+	return i8m.data
+}
+
+func (i8m *I8Matrix) GetCell(n, m int) (int8, error) {
+	if n <= i8m.n && m <= i8m.m {
+		return i8m.data[n][m], nil
+	}
+
+	return int8(-1), errors.New("Cantfcell")
+}
+
+func (i8m *I8Matrix) GetRow(rowNum) ([]int8, error) {
+	if rowNum <= i8m.n {
+		return i8m.data[rowNum], nil
+	}
+
+	return make([]int8, 0), errors.New("Cantfrow")
+}
+
+func (i8m *I8Matrix) GetColumn(colNum int) ([]int8, error) {
+	if colNum < i8m.m {
+		rv := make([]int8, n)
+		for i := 0; i < i8m.n; i++ {
+			rv[i] = i8m.data[i][colNum]
+		}
+
+		return rv, nil
+	}
+
+	return make([]int8, 0), errors.New("Cantfcol")
 }
